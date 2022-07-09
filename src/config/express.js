711e43e108b36c85c47713ab0eb9e2b443eead5e
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2022-07-09 11:13:21 -0300

    ✨ feat: user routes

--- src/config/express.js
+++ src/config/express.js
@@ -1,7 +1,10 @@
+import userRouter from '#Routes/user.routes.js';
 import express from 'express';
 
 const expressApp = express();
 
-// TODO Añadir los middlewares, rutas ...
+// TODO Añadir los middlewares globales a todas las request, rutas ...
+expressApp.use(express.json());
+expressApp.use('/user', userRouter);
 
 export default expressApp;
